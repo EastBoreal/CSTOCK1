57a2f3493b758efeadea8efb1cfa9cd903e9980e
EastBoreal <bruanin@gmail.com>
2023-02-24 14:29:26 +0900

    Delete data folder from the sync

---
+++ code/CSTOCK.R
@@ -0,0 +1,57 @@
+---
+  title: "CSTOCK"
+output:
+  word_document: default
+html_notebook: default
+---
+
+# Libraries ----------------------------------------------------------------------
+
+
+library(xlsx)
+library(readxl)
+library(psych)
+library(dplyr)
+library(ggplot2)
+library(factoextra)
+library(FactoMineR)
+library(ade4)
+library(car)
+library(corrplot)
+library(graphics)
+library(pvclust)
+library(doBy)
+library(ggpubr)
+library(scales)
+require(ggthemes)
+require(ggpmisc)
+library(beanplot)
+library(multcomp)
+require(multcomp)
+library(lubridate)
+library(MASS)
+library(TraMineR)
+library(vegan)
+library(tidyverse)
+library(rstatix)
+library(ggpubr)
+library(cowplot)
+library(gmt)
+library(geosphere)
+library(rlang)
+library(plyr)
+library(ggplot2)
+library(nlme)
+library(lme4) 
+library(gridExtra)
+library(GGally)
+library(devtools) 
+#install_github("vqv/ggbiplot")
+library(ggbiplot)
+library(reshape2)
+library(PMCMR)
+library(RVAideMemoire)
+library(MuMIn)
+library(visreg)
+
+```
